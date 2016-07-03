ff2cf115e48fdee50bec9946d38a414332920712
set <set@truedev.org>
2016-07-03 14:46:23 +0200

    Дописаны обработчики для удаления и подсветки

--- menu.js
+++ menu.js
@@ -3,29 +3,43 @@ class Menu {
 		this.el = options.el;
 
 		this._initEvents();
+		this.menuItems = options.el.querySelectorAll('.menu__item');
 	}
 
-	_initEvents () {
+	_initEvents() {
 		this.el.addEventListener('click', this._onMenuClick.bind(this));
 	}
 
 
-	_onMenuClick (event) {
-		let isItemClick = false;
-		if (event.target.classList.contains('menu__item')) {
-			isItemClick = true;
-			this._onMenuItemClick(event);
+	_onMenuClick(event) {
+		if (event.target.classList.contains('menu__title')) {
+			this.el.classList.toggle('menu_open');
 		}
 
-		if (!isItemClick) {
-			this.el.classList.toggle('menu_open');
-		} 
-		
+		if (event.target.classList.contains('menu__item')) {
+			this._onMenuItemClick(event);
+		}
+		if (event.target.classList.contains('menu__item-delete')) {
+			this._onMenuItemDelete(event);
+		}
+
+	}
+
+	_deleteActive() {
+		for (let i = 0; i < this.menuItems.length; i++) {
+			this.menuItems[i].classList.remove('menu__item_active');
+		}
+	}
+
+	_onMenuItemClick(event) {
+		this._deleteActive();
+		event.target.classList.add('menu__item_active');
 	}
 
 
-	_onMenuItemClick (event) {
-		console.log(event.target);
+	_onMenuItemDelete(event) {
+		console.log(this);
+		event.target.parentNode.parentNode.removeChild(event.target.parentNode);
 	}
 
 }
